fd944a52def16fab71d1eb1a10213077b82a0f28
Anatoliy Basharov <rexonka3@gmail.com>
2023-06-17 17:58:22 +0300

    Finish HomeTask2

--- HomeTask2/Program.cs
+++ HomeTask2/Program.cs
@@ -3,3 +3,23 @@
 //A (7,-5, 0); B (1,-1,9) -> 11.53
 
 
+int ReadInt(String message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+double Length(int x, int y, int z)
+{
+    double temp = (x * x) + (y * y) + (z * z);
+    return Math.Sqrt(temp);
+}
+int x1 = ReadInt("Введите координату Х первой точки ");
+int y1 = ReadInt("Введите координату y первой точки ");
+int z1 = ReadInt("Введите координату z первой точки ");
+int x2 = ReadInt("Введите координату Х второй точки ");
+int y2 = ReadInt("Введите координату y второй точки ");
+int z2 = ReadInt("Введите координату z второй точки ");
+
+double Distans = Length(x1-x2, y1-y2, z1-z2);
+System.Console.WriteLine($"Расстояние между двумя точками {Distans}");
