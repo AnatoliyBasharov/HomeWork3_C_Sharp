c7e64ca9ae28402fe68a783585fd8e3b97a61188
Anatoliy Basharov <rexonka3@gmail.com>
2023-06-17 18:11:57 +0300

    Finish HomeTask3

--- HomeTask3/Program.cs
+++ HomeTask3/Program.cs
@@ -3,3 +3,19 @@
 //5 -> 1, 8, 27, 64, 125
 
 
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message}");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int numb = ReadInt("Введите число ");
+int temp = 1;
+while (temp <= numb)
+{
+    int result = temp * temp * temp;
+    System.Console.Write($"{result} ");
+    temp++;
+}
